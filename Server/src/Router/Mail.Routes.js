f26f595f804dd57e8b8e410111b403b48ac199e6
kareen67 <karomi.edu@gmail.com>
2025-12-18 13:41:09 -0300

    .

--- Server/src/Router/Mail.Routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express")
-const { enviarNotificacion } = require("../Controller/Mail.Controller")
-
-const Ruta = express.Router()
-
-Ruta.post("/notificacion", enviarNotificacion)
-
-module.exports = Ruta
